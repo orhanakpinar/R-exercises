1454a5f1db557ec2e54ce1ee9a3331bf76be1329
Orhan <63443864+orhanakpinar@users.noreply.github.com>
2022-12-08 22:11:11 +0100

    Update Soc334-Odev21.R

---
+++ Soc334-Odev21.R
@@ -0,0 +1,23 @@
+library(knitr)
+install.packages('ggplot2', repos = "http://cran.us.r-project.org")
+library(ggplot2)
+
+
+head(edusavings)
+
+summary(edusavings)
+
+sd(edusavings$Education.expenditure.Percent.of.GNI.2019)
+sd(edusavings$Unemployment.with.advanced.education.Percent.of.total.labor.force.2019)
+
+hist(edusavings$Education.expenditure.Percent.of.GNI.2019)
+hist(edusavings$Unemployment.with.advanced.education.Percent.of.total.labor.force.2019)
+
+ggplot(edusavings,aes(x=Unemployment.with.advanced.education.Percent.of.total.labor.force.2019,y=Education.expenditure.Percent.of.GNI.2019))+
+  geom_point()+
+  geom_smooth(method = "lm")
+
+lm(formula=Unemployment.with.advanced.education.Percent.of.total.labor.force.2019~Education.expenditure.Percent.of.GNI.2019,data=edusavings)
+
+fit <- lm(formula=Unemployment.with.advanced.education.Percent.of.total.labor.force.2019~Education.expenditure.Percent.of.GNI.2019,data=edusavings)
+summary(fit)
