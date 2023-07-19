28b24223e9d176d06407186309a0705cbab30c53
orhanakpinar <orhan.akpinar1@gmail.com>
2023-07-19 18:48:13 +0300

    First commit from my local computer

--- live_coding_R.R
+++ live_coding_R.R
@@ -13,3 +13,5 @@
 
 #I still couldn't figured it out, having problems about my administrator.
 #I also might need to arrange some windows system files.
+#is this first commit? 
+#is this first commit? 
